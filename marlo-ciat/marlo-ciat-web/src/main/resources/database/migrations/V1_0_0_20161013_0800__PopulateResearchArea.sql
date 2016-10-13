e0649b9cfb595e396c6f77fddcd00bfbe2a8b171
nmatovu <noahmatovu@yahoo.com>
2016-10-13 16:13:27 +0300

    Updated migration scripts with id values.

--- marlo-ciat/marlo-ciat-web/src/main/resources/database/migrations/V1_0_0_20161013_0800__PopulateResearchArea.sql
+++ marlo-ciat/marlo-ciat-web/src/main/resources/database/migrations/V1_0_0_20161013_0800__PopulateResearchArea.sql
@@ -1,3 +1,3 @@
-INSERT INTO `research_areas` (`name`, `acronym`, `research_center_id`) VALUES ('Decision and Policy Analysis', 'DAPA', 1);
-INSERT INTO `research_areas` (`name`, `acronym`, `research_center_id`) VALUES ('Agrobiodiversity', 'AgBIO', 1);
-INSERT INTO `research_areas` (`name`, `acronym`, `research_center_id`) VALUES ('Soils', 'SOILS', 1);
+INSERT INTO `research_areas` (`id`,`name`, `acronym`, `research_center_id`) VALUES (1,'Decision and Policy Analysis', 'DAPA', 1);
+INSERT INTO `research_areas` (`id`,`name`, `acronym`, `research_center_id`) VALUES (2,'Agrobiodiversity', 'AgBIO', 1);
+INSERT INTO `research_areas` (`id`,`name`, `acronym`, `research_center_id`) VALUES (3,'Soils', 'SOILS', 1);
--- marlo-ciat/marlo-ciat-web/src/main/resources/database/migrations/V1_0_0_20161013_0830__PopulatePrograms.sql
+++ marlo-ciat/marlo-ciat-web/src/main/resources/database/migrations/V1_0_0_20161013_0830__PopulatePrograms.sql
@@ -1,3 +1,3 @@
-INSERT INTO `research_programs` (`name`, `acronym`, `program_type`, `is_active`, `research_area_id`) VALUES ('Linking Farmers to Markets', 'LFM', 3, 1, 1);
-INSERT INTO `research_programs` (`name`, `acronym`, `program_type`, `is_active`, `research_area_id`) VALUES ('Climate Change', 'CC', 1, 1, 1);
-INSERT INTO `research_programs` (`name`, `acronym`, `program_type`, `is_active`, `research_area_id`) VALUES ('Ecosystem Service', 'ES', 2, 1, 1);
+INSERT INTO `research_programs` (`id`,`name`, `acronym`, `program_type`, `is_active`, `research_area_id`) VALUES (1,'Linking Farmers to Markets', 'LFM', 3, 1, 1);
+INSERT INTO `research_programs` (`id`,`name`, `acronym`, `program_type`, `is_active`, `research_area_id`) VALUES (2,'Climate Change', 'CC', 1, 1, 1);
+INSERT INTO `research_programs` (`id`,`name`, `acronym`, `program_type`, `is_active`, `research_area_id`) VALUES (3,'Ecosystem Service', 'ES', 2, 1, 1);
