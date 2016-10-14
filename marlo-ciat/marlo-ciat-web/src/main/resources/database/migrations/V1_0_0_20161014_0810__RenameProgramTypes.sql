277ec0c36be258921eeee2b8c8ee9c82946a0315
nmatovu <nmatovu@leonleah>
2016-10-14 09:13:39 +0300

    Updated research program names values.

---
+++ marlo-ciat/marlo-ciat-web/src/main/resources/database/migrations/V1_0_0_20161014_0810__RenameProgramTypes.sql
@@ -0,0 +1,3 @@
+UPDATE `all_types` SET `type_name`='FLAGSHIP_PROGRAM_TYPE' WHERE `id`='2';
+UPDATE `all_types` SET `type_name`='REGIONAL_PROGRAM_TYPE' WHERE `id`='1';
+DELETE FROM `all_types` WHERE `id`='3';
