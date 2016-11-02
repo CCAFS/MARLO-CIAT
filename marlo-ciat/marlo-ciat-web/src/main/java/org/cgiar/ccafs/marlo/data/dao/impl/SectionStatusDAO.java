4b6c0e36b0ccb43a6c30a22846cbe0b85542d1da
Hermes Jimenez <h.jimenez@cgiar.org>
2016-11-02 16:20:12 -0500

    Implement the section statuses model. closed #53

---
+++ marlo-ciat/marlo-ciat-web/src/main/java/org/cgiar/ccafs/marlo/data/dao/impl/SectionStatusDAO.java
@@ -0,0 +1,96 @@
+/*****************************************************************
+ * This file is part of Managing Agricultural Research for Learning &
+ * Outcomes Platform (MARLO).
+ * MARLO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * MARLO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with MARLO. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+
+
+package org.cgiar.ccafs.marlo.data.dao.impl;
+
+import org.cgiar.ccafs.marlo.data.dao.ISectionStatusDAO;
+import org.cgiar.ccafs.marlo.data.model.SectionStatus;
+
+import java.util.List;
+
+import com.google.inject.Inject;
+
+public class SectionStatusDAO implements ISectionStatusDAO {
+
+  private StandardDAO dao;
+
+  @Inject
+  public SectionStatusDAO(StandardDAO dao) {
+    this.dao = dao;
+  }
+
+  @Override
+  public boolean deleteSectionStatus(long sectionStatusId) {
+    SectionStatus sectionStatus = this.find(sectionStatusId);
+    return dao.delete(sectionStatus);
+  }
+
+  @Override
+  public boolean existSectionStatus(long sectionStatusID) {
+    SectionStatus sectionStatus = this.find(sectionStatusID);
+    if (sectionStatus == null) {
+      return false;
+    }
+    return true;
+
+  }
+
+  @Override
+  public SectionStatus find(long id) {
+    return dao.find(SectionStatus.class, id);
+
+  }
+
+  @Override
+  public List<SectionStatus> findAll() {
+    String query = "from " + SectionStatus.class.getName();
+    List<SectionStatus> list = dao.findAll(query);
+    if (list.size() > 0) {
+      return list;
+    }
+    return null;
+
+  }
+
+  @Override
+  public SectionStatus getSectionStatusByProgram(long programId, String sectionName) {
+    String query = "from " + SectionStatus.class.getName() + " where section_name='" + sectionName
+      + "' and research_program_id=" + programId;
+    List<SectionStatus> list = dao.findAll(query);
+    if (list.size() > 0) {
+      return list.get(0);
+    }
+    return null;
+  }
+
+  @Override
+  public List<SectionStatus> getSectionStatussByUserId(long userId) {
+    String query = "from " + SectionStatus.class.getName() + " where user_id=" + userId;
+    return dao.findAll(query);
+  }
+
+  @Override
+  public long save(SectionStatus sectionStatus) {
+    if (sectionStatus.getId() == null) {
+      dao.save(sectionStatus);
+    } else {
+      dao.update(sectionStatus);
+    }
+    return sectionStatus.getId();
+  }
+
+
+}
---
+++ marlo-ciat/marlo-ciat-web/src/main/java/org/cgiar/ccafs/marlo/data/model/SectionStatus.java
@@ -0,0 +1,99 @@
+/*****************************************************************
+ * This file is part of Managing Agricultural Research for Learning &
+ * Outcomes Platform (MARLO).
+ * MARLO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * MARLO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with MARLO. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+package org.cgiar.ccafs.marlo.data.model;
+
+
+public class SectionStatus implements java.io.Serializable {
+
+
+  private static final long serialVersionUID = -5830242271701358980L;
+
+
+  private Long id;
+
+  private ResearchProgram researchProgram;
+  private String sectionName;
+  private String missingFields;
+  private String cycle;
+  private Integer year;
+
+  public SectionStatus() {
+  }
+
+  public SectionStatus(ResearchProgram researchProgram, String sectionName, String missingFields, String cycle,
+    Integer year) {
+    this.researchProgram = researchProgram;
+    this.sectionName = sectionName;
+    this.missingFields = missingFields;
+    this.cycle = cycle;
+    this.year = year;
+  }
+
+  public SectionStatus(String sectionName) {
+    this.sectionName = sectionName;
+  }
+
+
+  public String getCycle() {
+    return this.cycle;
+  }
+
+  public Long getId() {
+    return this.id;
+  }
+
+  public String getMissingFields() {
+    return this.missingFields;
+  }
+
+  public ResearchProgram getResearchProgram() {
+    return researchProgram;
+  }
+
+  public String getSectionName() {
+    return this.sectionName;
+  }
+
+  public Integer getYear() {
+    return this.year;
+  }
+
+  public void setCycle(String cycle) {
+    this.cycle = cycle;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public void setMissingFields(String missingFields) {
+    this.missingFields = missingFields;
+  }
+
+  public void setResearchProgram(ResearchProgram researchProgram) {
+    this.researchProgram = researchProgram;
+  }
+
+  public void setSectionName(String sectionName) {
+    this.sectionName = sectionName;
+  }
+
+  public void setYear(Integer year) {
+    this.year = year;
+  }
+
+
+}
+
