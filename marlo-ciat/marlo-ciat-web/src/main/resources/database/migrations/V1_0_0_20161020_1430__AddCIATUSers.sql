650eb899ec946fc6ac7e779be8c4d87e89c42ca9
Hermes Jimenez <h.jimenez@cgiar.org>
2016-11-22 10:35:34 -0500

    Update data migration.

--- marlo-ciat/marlo-ciat-web/src/main/resources/database/migrations/V1_0_0_20161020_1430__AddCIATUSers.sql
+++ marlo-ciat/marlo-ciat-web/src/main/resources/database/migrations/V1_0_0_20161020_1430__AddCIATUSers.sql
@@ -21,9 +21,3 @@ where id = 843;
 
 UPDATE users set is_active=0, is_cgiar_user= 1
 where id = 1108;
-
-INSERT INTO `users` VALUES ('1109', 'Marcela', 'Quintero', 'mquintero', 'm.quintero@cgiar.org', null, '1', '3', '2016-10-20 14:19:07', '3', ' ', '1', null, '1');
-INSERT INTO `users` VALUES ('1110', 'Joe', 'Tohme', 'jtohme', 'j.tohme2@cgiar.org', null, '1', '3', '2016-10-20 14:26:31', '3', ' ', '1', null, '1');
-INSERT INTO `users` VALUES ('1111', 'Luis Augusto', 'Becerra Lopez-Lavalle', 'labecerra', 'l.a.becerra@cgiar.org', null, '1', '3', '2016-10-20 14:28:55', '3', ' ', '1', null, '1');
-INSERT INTO `users` VALUES ('1112', 'Fernando', 'Correa', 'fcorrea', 'f.correa@cgiar.org', null, '1', '3', '2016-10-20 14:33:37', '3', ' ', '1', null, '1');
-INSERT INTO `users` VALUES ('1113', 'Daniel', 'Debouck', 'ddebouck', 'd.debouck@cgiar.org', null, '1', '3', '2016-10-20 14:34:49', '3', ' ', '1', null, '1');
