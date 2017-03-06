aab20b174702101a0673e07233a71d0a34b9e534
Hermes Jimenez <h.jimenez@cgiar.org>
2017-03-06 11:55:26 -0500

    Working in Project Description validator. issue #156

--- marlo-ciat/marlo-ciat-web/src/main/java/org/cgiar/ccafs/marlo/validation/monitoring/project/ProjectDescriptionValidator.java
+++ marlo-ciat/marlo-ciat-web/src/main/java/org/cgiar/ccafs/marlo/validation/monitoring/project/ProjectDescriptionValidator.java
@@ -17,6 +17,7 @@ package org.cgiar.ccafs.marlo.validation.monitoring.project;
 
 import org.cgiar.ccafs.marlo.action.BaseAction;
 import org.cgiar.ccafs.marlo.data.model.Project;
+import org.cgiar.ccafs.marlo.data.model.ProjectFundingSource;
 import org.cgiar.ccafs.marlo.data.model.ResearchProgram;
 import org.cgiar.ccafs.marlo.utils.InvalidFieldsMessages;
 import org.cgiar.ccafs.marlo.validation.BaseValidator;
@@ -44,19 +45,74 @@ public class ProjectDescriptionValidator extends BaseValidator {
 
   }
 
+  public void validateFundingSource(BaseAction baseAction, ProjectFundingSource fundingSource, int i) {
+    if (fundingSource.getFundingSourceType() == null) {
+      this.addMessage(
+        baseAction.getText("projectDescription.action.fundingSources.fundingSourceType.id", String.valueOf(i + 1)));
+      baseAction.getInvalidFields().put("input-project.fundingSources[" + i + "].fundingSourceType.id",
+        InvalidFieldsMessages.EMPTYFIELD);
+    } else {
+      if (fundingSource.getFundingSourceType().getId() == null || fundingSource.getFundingSourceType().getId() == -1) {
+        this.addMessage(
+          baseAction.getText("projectDescription.action.fundingSources.fundingSourceType.id", String.valueOf(i + 1)));
+        baseAction.getInvalidFields().put("input-project.fundingSources[" + i + "].fundingSourceType.id",
+          InvalidFieldsMessages.EMPTYFIELD);
+      }
+    }
+
+    if (!this.isValidString(fundingSource.getDonor()) && this.wordCount(fundingSource.getDonor()) <= 100) {
+      this.addMessage(
+        baseAction.getText("projectDescription.action.fundingSources.fundingSourceType.donor", String.valueOf(i + 1)));
+      baseAction.getInvalidFields().put("input-project.fundingSources[" + i + "].fundingSourceType.donor",
+        InvalidFieldsMessages.EMPTYFIELD);
+    }
+  }
+
+
   public void validateProjectDescription(BaseAction baseAction, Project project) {
 
+    if (!this.isValidString(project.getName()) && this.wordCount(project.getName()) <= 150) {
+      this.addMessage(baseAction.getText("projectDescription.action.title"));
+      baseAction.getInvalidFields().put("input-project.name", InvalidFieldsMessages.EMPTYFIELD);
+    }
+
+    if (!this.isValidString(project.getShortName()) && this.wordCount(project.getShortName()) <= 50) {
+      this.addMessage(baseAction.getText("projectDescription.action.shortName"));
+      baseAction.getInvalidFields().put("input-project.shortName", InvalidFieldsMessages.EMPTYFIELD);
+    }
+
+    if (project.getStartDate() == null) {
+      this.addMessage(baseAction.getText("projectDescription.action.startDate"));
+      baseAction.getInvalidFields().put("input-project.startDate", InvalidFieldsMessages.EMPTYFIELD);
+    }
+    if (project.getEndDate() == null) {
+      this.addMessage(baseAction.getText("projectDescription.action.endDate"));
+      baseAction.getInvalidFields().put("input-project.endDate", InvalidFieldsMessages.EMPTYFIELD);
+    }
+
+    if (project.getProjectLeader() == null) {
+      this.addMessage(baseAction.getText("projectDescription.action.projectLeader"));
+      baseAction.getInvalidFields().put("input-project.projectLeader", InvalidFieldsMessages.EMPTYFIELD);
+    }
+
+    // TODO project.getProjectCrosscutingTheme() Validation
+
+
     if (project.getFundingSources() == null || project.getFundingSources().isEmpty()) {
-      this.addMessage(baseAction.getText("outcome.action.milestones"));
-      baseAction.getInvalidFields().put("list-outcome.milestones",
-        baseAction.getText(InvalidFieldsMessages.EMPTYLIST, new String[] {"Milestones"}));
+      this.addMessage(baseAction.getText("projectDescription.action.fundingSources"));
+      baseAction.getInvalidFields().put("list-project.fundingSources",
+        baseAction.getText(InvalidFieldsMessages.EMPTYLIST));
     } else {
       for (int i = 0; i < project.getFundingSources().size(); i++) {
-        // TODO
-        // this.validateMilestones(baseAction, outcome.getMilestones().get(i), i);
+        this.validateFundingSource(baseAction, project.getFundingSources().get(i), i);
       }
-
     }
+
+    if (project.getOutputs() == null || project.getOutputs().isEmpty()) {
+      this.addMessage(baseAction.getText("projectDescription.actio.outputs"));
+      baseAction.getInvalidFields().put("list-project.outputs", baseAction.getText(InvalidFieldsMessages.EMPTYLIST));
+    }
+
 
   }
 
