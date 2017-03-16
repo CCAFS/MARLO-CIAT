f015e16712221ec53685615eacb606a8c1392668
Hermes Jimenez <h.jimenez@cgiar.org>
2017-03-16 15:53:28 -0500

    issue #168

--- marlo-ciat/marlo-ciat-web/src/main/java/org/cgiar/ccafs/marlo/action/monitoring/outcome/MonitoringOutcomeAction.java
+++ marlo-ciat/marlo-ciat-web/src/main/java/org/cgiar/ccafs/marlo/action/monitoring/outcome/MonitoringOutcomeAction.java
@@ -17,13 +17,16 @@ package org.cgiar.ccafs.marlo.action.monitoring.outcome;
 
 import org.cgiar.ccafs.marlo.action.BaseAction;
 import org.cgiar.ccafs.marlo.config.APConfig;
+import org.cgiar.ccafs.marlo.data.model.MonitoringMilestone;
 import org.cgiar.ccafs.marlo.data.model.MonitoringOutcome;
 import org.cgiar.ccafs.marlo.data.model.ResearchArea;
 import org.cgiar.ccafs.marlo.data.model.ResearchCenter;
 import org.cgiar.ccafs.marlo.data.model.ResearchImpact;
+import org.cgiar.ccafs.marlo.data.model.ResearchMilestone;
 import org.cgiar.ccafs.marlo.data.model.ResearchOutcome;
 import org.cgiar.ccafs.marlo.data.model.ResearchProgram;
 import org.cgiar.ccafs.marlo.data.model.ResearchTopic;
+import org.cgiar.ccafs.marlo.data.model.TargetUnit;
 import org.cgiar.ccafs.marlo.data.service.IAuditLogService;
 import org.cgiar.ccafs.marlo.data.service.ICenterService;
 import org.cgiar.ccafs.marlo.data.service.IProgramService;
@@ -35,7 +38,13 @@ import org.cgiar.ccafs.marlo.data.service.ITargetUnitService;
 import org.cgiar.ccafs.marlo.utils.APConstants;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -52,15 +61,9 @@ public class MonitoringOutcomeAction extends BaseAction {
 
 
   private ICenterService centerService;
-
   private IResearchOutcomeService outcomeService;
-
-
   private IAuditLogService auditLogService;
-
   private ITargetUnitService targetUnitService;
-
-
   private IResearchTopicService researchTopicService;
   private IProgramService programService;
   private IResearchImpactService impactService;
@@ -75,7 +78,6 @@ public class MonitoringOutcomeAction extends BaseAction {
   private List<ResearchTopic> researchTopics;
   private ResearchTopic selectedResearchTopic;
   private List<ResearchImpact> researchImpacts;
-  private List<MonitoringOutcome> monitoringOutcomes;
   private HashMap<Long, String> targetUnitList;
   // Parameter Variables
   private long programID;
@@ -99,27 +101,84 @@ public class MonitoringOutcomeAction extends BaseAction {
     this.auditLogService = auditLogService;
   }
 
+  public void fillFrontValues() {
+    programID = outcome.getResearchTopic().getResearchProgram().getId();
+    selectedProgram = programService.getProgramById(programID);
+    selectedResearchTopic = outcome.getResearchTopic();
+    topicID = selectedResearchTopic.getId();
+    selectedResearchArea = selectedProgram.getResearchArea();
+    areaID = selectedResearchArea.getId();
+
+    targetUnitList = new HashMap<>();
+    if (targetUnitService.findAll() != null) {
+
+      List<TargetUnit> targetUnits =
+        targetUnitService.findAll().stream().filter(c -> c.isActive()).collect(Collectors.toList());
+
+      Collections.sort(targetUnits,
+        (tu1, tu2) -> tu1.getName().toLowerCase().trim().compareTo(tu2.getName().toLowerCase().trim()));
+
+      for (TargetUnit srfTargetUnit : targetUnits) {
+        targetUnitList.put(srfTargetUnit.getId(), srfTargetUnit.getName());
+      }
+
+      targetUnitList = this.sortByComparator(targetUnitList);
+    }
+  }
+
+  public void fillOutcomeYear() {
+    outcome.setMonitorings(new ArrayList<>());
+
+    Calendar calendarStart = Calendar.getInstance();
+    calendarStart.set(Calendar.YEAR, this.getYear());
+    Calendar calendarEnd = Calendar.getInstance();
+    calendarEnd.set(Calendar.YEAR, outcome.getTargetYear());
+
+    while (calendarStart.get(Calendar.YEAR) <= calendarEnd.get(Calendar.YEAR)) {
+      MonitoringOutcome monitoringOutcome = new MonitoringOutcome();
+
+      monitoringOutcome.setId(new Long(-1));
+      monitoringOutcome.setActive(true);
+      monitoringOutcome.setYear(calendarStart.get(Calendar.YEAR));
+
+      monitoringOutcome.setMilestones(new ArrayList<>());
+
+
+      List<ResearchMilestone> milestones = new ArrayList<>(
+        outcome.getResearchMilestones().stream().filter(rm -> rm.isActive()).collect(Collectors.toList()));
+
+      for (ResearchMilestone researchMilestone : milestones) {
+
+        MonitoringMilestone monitoringMilestone = new MonitoringMilestone();
+        monitoringMilestone.setId(new Long(-1));
+        monitoringMilestone.setActive(true);
+        monitoringMilestone.setResearchMilestone(researchMilestone);
+
+        monitoringOutcome.getMilestones().add(monitoringMilestone);
+
+      }
+
+      outcome.getMonitorings().add(monitoringOutcome);
+    }
+  }
+
   public long getAreaID() {
     return areaID;
   }
+
 
   public ResearchCenter getLoggedCenter() {
     return loggedCenter;
   }
 
-  public List<MonitoringOutcome> getMonitoringOutcomes() {
-    return monitoringOutcomes;
-  }
 
   public ResearchOutcome getOutcome() {
     return outcome;
   }
 
-
   public long getOutcomeID() {
     return outcomeID;
   }
-
 
   public long getProgramID() {
     return programID;
@@ -129,6 +188,7 @@ public class MonitoringOutcomeAction extends BaseAction {
     return researchAreas;
   }
 
+
   public List<ResearchImpact> getResearchImpacts() {
     return researchImpacts;
   }
@@ -137,14 +197,15 @@ public class MonitoringOutcomeAction extends BaseAction {
     return researchPrograms;
   }
 
-
   public List<ResearchTopic> getResearchTopics() {
     return researchTopics;
   }
 
+
   public ResearchProgram getSelectedProgram() {
     return selectedProgram;
   }
+
 
   public ResearchArea getSelectedResearchArea() {
     return selectedResearchArea;
@@ -165,7 +226,6 @@ public class MonitoringOutcomeAction extends BaseAction {
     return topicID;
   }
 
-
   @Override
   public void prepare() throws Exception {
     areaID = -1;
@@ -183,15 +243,28 @@ public class MonitoringOutcomeAction extends BaseAction {
 
     outcome = outcomeService.getResearchOutcomeById(outcomeID);
 
-    outcome.setMonitorings(new ArrayList<>(
-      outcome.getMonitoringOutcomes().stream().filter(mo -> mo.isActive()).collect(Collectors.toList())));
+    researchAreas = new ArrayList<>(
+      loggedCenter.getResearchAreas().stream().filter(ra -> ra.isActive()).collect(Collectors.toList()));
 
-    if (outcome.getMonitorings() == null || outcome.getMonitorings().isEmpty()) {
+    Collections.sort(researchAreas, (ra1, ra2) -> ra1.getId().compareTo(ra2.getId()));
+
+
+    if (outcome != null) {
+
+
+      this.fillFrontValues();
+
+      outcome.setMonitorings(new ArrayList<>(
+        outcome.getMonitoringOutcomes().stream().filter(mo -> mo.isActive()).collect(Collectors.toList())));
+
+      if (outcome.getMonitorings() == null || outcome.getMonitorings().isEmpty()) {
+        this.fillOutcomeYear();
+      }
+
 
     }
 
   }
-
 
   public void setAreaID(long areaID) {
     this.areaID = areaID;
@@ -200,11 +273,6 @@ public class MonitoringOutcomeAction extends BaseAction {
 
   public void setLoggedCenter(ResearchCenter loggedCenter) {
     this.loggedCenter = loggedCenter;
-  }
-
-
-  public void setMonitoringOutcomes(List<MonitoringOutcome> monitoringOutcomes) {
-    this.monitoringOutcomes = monitoringOutcomes;
   }
 
 
@@ -254,6 +322,35 @@ public class MonitoringOutcomeAction extends BaseAction {
 
   public void setTopicID(long topicID) {
     this.topicID = topicID;
+  }
+
+  /**
+   * method that sort a map list alphabetical
+   * 
+   * @param unsortMap - map to sort
+   * @return
+   */
+  private HashMap<Long, String> sortByComparator(HashMap<Long, String> unsortMap) {
+
+    // Convert Map to List
+    List<HashMap.Entry<Long, String>> list = new LinkedList<HashMap.Entry<Long, String>>(unsortMap.entrySet());
+
+    // Sort list with comparator, to compare the Map values
+    Collections.sort(list, new Comparator<HashMap.Entry<Long, String>>() {
+
+      @Override
+      public int compare(HashMap.Entry<Long, String> o1, HashMap.Entry<Long, String> o2) {
+        return (o1.getValue().toLowerCase().trim()).compareTo(o2.getValue().toLowerCase().trim());
+      }
+    });
+
+    // Convert sorted map back to a Map
+    HashMap<Long, String> sortedMap = new LinkedHashMap<Long, String>();
+    for (Iterator<HashMap.Entry<Long, String>> it = list.iterator(); it.hasNext();) {
+      HashMap.Entry<Long, String> entry = it.next();
+      sortedMap.put(entry.getKey(), entry.getValue());
+    }
+    return sortedMap;
   }
 
 }
