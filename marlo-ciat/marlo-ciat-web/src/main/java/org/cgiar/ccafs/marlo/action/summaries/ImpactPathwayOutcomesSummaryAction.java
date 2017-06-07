5fd49a2e2933c30e3ee67cff67e0737678523127
AndresFVR <a.valencia@cgiar.org>
2017-06-07 10:48:04 -0500

    Filter impactPathwayOutcomes by program

--- marlo-ciat/marlo-ciat-web/src/main/java/org/cgiar/ccafs/marlo/action/summaries/ImpactPathwayOutcomesSummaryAction.java
+++ marlo-ciat/marlo-ciat-web/src/main/java/org/cgiar/ccafs/marlo/action/summaries/ImpactPathwayOutcomesSummaryAction.java
@@ -17,15 +17,8 @@ package org.cgiar.ccafs.marlo.action.summaries;
 import org.cgiar.ccafs.marlo.action.BaseAction;
 import org.cgiar.ccafs.marlo.config.APConfig;
 import org.cgiar.ccafs.marlo.config.PentahoListener;
-import org.cgiar.ccafs.marlo.data.model.ResearchCycle;
-import org.cgiar.ccafs.marlo.data.model.ResearchImpact;
-import org.cgiar.ccafs.marlo.data.model.ResearchImpactObjective;
-import org.cgiar.ccafs.marlo.data.model.ResearchOutcome;
-import org.cgiar.ccafs.marlo.data.model.ResearchOutput;
 import org.cgiar.ccafs.marlo.data.model.ResearchProgram;
-import org.cgiar.ccafs.marlo.data.model.ResearchTopic;
 import org.cgiar.ccafs.marlo.data.service.IProgramService;
-import org.cgiar.ccafs.marlo.data.service.IResearchCycleService;
 import org.cgiar.ccafs.marlo.utils.APConstants;
 
 import java.io.ByteArrayInputStream;
@@ -37,7 +30,6 @@ import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.stream.Collectors;
 
 import com.google.inject.Inject;
 import org.apache.commons.lang3.StringUtils;
@@ -71,18 +63,14 @@ public class ImpactPathwayOutcomesSummaryAction extends BaseAction implements Su
   private byte[] bytesPDF;
   // Services
   private IProgramService programService;
-  private IResearchCycleService cycleService;
   // Params
   private ResearchProgram researchProgram;
-  private ResearchCycle researchCycle;
   private long startTime;
 
   @Inject
-  public ImpactPathwayOutcomesSummaryAction(APConfig config, IProgramService programService,
-    IResearchCycleService cycleService) {
+  public ImpactPathwayOutcomesSummaryAction(APConfig config, IProgramService programService) {
     super(config);
     this.programService = programService;
-    this.cycleService = cycleService;
   }
 
   @Override
@@ -137,31 +125,6 @@ public class ImpactPathwayOutcomesSummaryAction extends BaseAction implements Su
     return SUCCESS;
 
   }
-
-
-  private void fillSubreport(SubReport subReport, String query) {
-    CompoundDataFactory cdf = CompoundDataFactory.normalize(subReport.getDataFactory());
-    TableDataFactory sdf = (TableDataFactory) cdf.getDataFactoryForQuery(query);
-    TypedTableModel model = null;
-    switch (query) {
-      case "program_impact":
-        model = this.getProgramImpactTableModel();
-        break;
-      // case "research_topics":
-      // model = this.getResearchTopicsTableModel();
-      // break;
-      case "outcomes":
-        model = this.getOutcomesTableModel();
-        break;
-      case "outputs":
-        model = this.getOutputsTableModel();
-        break;
-
-    }
-    sdf.addTable(query, model);
-    subReport.setDataFactory(cdf);
-  }
-
 
   /**
    * Get all subreports and store then in a hash map.
@@ -231,7 +194,6 @@ public class ImpactPathwayOutcomesSummaryAction extends BaseAction implements Su
   }
 
   private File getFile(String fileName) {
-
     // Get file from resources folder
     ClassLoader classLoader = this.getClass().getClassLoader();
     File file = new File(classLoader.getResource(fileName).getFile());
@@ -281,8 +243,9 @@ public class ImpactPathwayOutcomesSummaryAction extends BaseAction implements Su
    */
   private TypedTableModel getMasterTableModel() {
     // Initialization of Model
-    TypedTableModel model = new TypedTableModel(new String[] {"title", "current_date", "imageUrl"},
-      new Class[] {String.class, String.class, String.class});
+    TypedTableModel model =
+      new TypedTableModel(new String[] {"title", "current_date", "imageUrl", "research_program_id"},
+        new Class[] {String.class, String.class, String.class, Long.class});
     String title = "Impact Pathway Full Report";
     String currentDate = "";
 
@@ -297,235 +260,13 @@ public class ImpactPathwayOutcomesSummaryAction extends BaseAction implements Su
     // Get CIAT imgage URL from repo
     String imageUrl = this.getBaseUrl() + "/images/global/centers/CIAT.png";
 
-    model.addRow(new Object[] {title, currentDate, imageUrl});
-    return model;
-  }
 
-  private TypedTableModel getOutcomesTableModel() {
-    // Initialization of Model
-    TypedTableModel model = new TypedTableModel(
-      new String[] {"shortName", "statement", "research_topic", "program_impact", "target_unit", "target_value",
-        "target_year", "showResearchTopic", "id"},
-      new Class[] {String.class, String.class, String.class, String.class, String.class, String.class, String.class,
-        Boolean.class, Long.class});
-
-
-    // Get research topics and then outcomes
-    for (ResearchTopic researchTopic : researchProgram.getResearchTopics().stream()
-      .filter(rt -> rt.isActive() && rt.getResearchOutcomes().size() > 0).collect(Collectors.toList())) {
-      String researchTopicTitle = "";
-      Boolean showResearchTopic;
-      int countOutcome = 0;
-      for (ResearchOutcome researchOutcome : researchTopic.getResearchOutcomes().stream().filter(ro -> ro.isActive())
-        .collect(Collectors.toList())) {
-        if (countOutcome == 0) {
-          if (researchTopic.getResearchTopic() != null) {
-            researchTopicTitle = researchTopic.getResearchTopic();
-          }
-          if (researchTopicTitle.isEmpty()) {
-            researchTopicTitle = "&lt;Not Defined&gt;";
-          }
-          showResearchTopic = true;
-        } else {
-          showResearchTopic = false;
-        }
-
-
-        String shortName = "";
-        String statement = "";
-
-        String programImpact = "";
-        String targetUnit = "";
-        String targetValue = "";
-        String targetYear = "";
-        Long id = researchOutcome.getId();
-
-        if (researchOutcome.getShortName() != null) {
-          shortName = researchOutcome.getShortName();
-        }
-        if (shortName.isEmpty()) {
-          shortName = "&lt;Not Defined&gt;";
-        }
-
-        if (researchOutcome.getDescription() != null) {
-          statement = researchOutcome.getDescription();
-        }
-        if (statement.isEmpty()) {
-          statement = "&lt;Not Defined&gt;";
-        }
-
-        if (researchOutcome.getResearchImpact() != null) {
-          programImpact = researchOutcome.getResearchImpact().getDescription();
-        }
-        if (programImpact.isEmpty()) {
-          programImpact = "&lt;Not Defined&gt;";
-        }
-        if (researchOutcome.getTargetUnit() != null) {
-          targetUnit = researchOutcome.getTargetUnit().getName();
-        }
-        if (targetUnit.isEmpty()) {
-          targetUnit = "&lt;Not Defined&gt;";
-        }
-
-        if (targetUnit.equals("Not Applicable")) {
-          targetValue = "Not Applicable";
-        } else {
-          if (researchOutcome.getValue() != null) {
-            targetValue = researchOutcome.getValue().toString();
-          }
-        }
-
-        if (targetValue == null || targetValue.isEmpty()) {
-          targetValue = "&lt;Not Defined&gt;";
-        }
-        if (researchOutcome.getTargetYear() != null) {
-          targetYear = researchOutcome.getTargetYear().toString();
-        }
-        if (targetYear.isEmpty()) {
-          targetYear = "&lt;Not Defined&gt;";
-        }
-
-        countOutcome++;
-        model.addRow(new Object[] {shortName, statement, researchTopicTitle, programImpact, targetUnit, targetValue,
-          targetYear, showResearchTopic, id});
-      }
-
-    }
-
-
-    return model;
-  }
-
-  private TypedTableModel getOutputsTableModel() {
-    // Initialization of Model
-    TypedTableModel model =
-      new TypedTableModel(new String[] {"shortName", "title", "research_topic", "outcome", "showResearchTopic", "id"},
-        new Class[] {String.class, String.class, String.class, String.class, Boolean.class, Long.class});
-
-    for (ResearchTopic researchTopic : researchProgram.getResearchTopics().stream().filter(rt -> rt.isActive())
-      .collect(Collectors.toList())) {
-      String researchTopicTitle = "";
-      Boolean showResearchTopic;
-      int countOutcome = 0;
-      for (ResearchOutcome researchOutcome : researchTopic.getResearchOutcomes().stream().filter(ro -> ro.isActive())
-        .collect(Collectors.toList())) {
-        for (ResearchOutput researchOutput : researchOutcome.getResearchOutputs().stream().filter(ro -> ro.isActive())
-          .collect(Collectors.toList())) {
-          Long id = researchOutput.getId();
-          if (countOutcome == 0) {
-            if (researchTopic.getResearchTopic() != null) {
-              researchTopicTitle = researchTopic.getResearchTopic();
-            }
-            if (researchTopicTitle.isEmpty()) {
-              researchTopicTitle = "&lt;Not Defined&gt;";
-            }
-            showResearchTopic = true;
-          } else {
-            showResearchTopic = false;
-          }
-
-          String shortName = "";
-          String title = "";
-          String outcome = "";
-
-          if (researchOutput.getShortName() != null) {
-            shortName = researchOutput.getShortName();
-          }
-          if (shortName.isEmpty()) {
-            shortName = "&lt;Not Defined&gt;";
-          }
-          if (researchOutput.getTitle() != null) {
-            title = researchOutput.getTitle();
-          }
-          if (title.isEmpty()) {
-            title = "&lt;Not Defined&gt;";
-          }
-
-
-          if (researchOutcome.getDescription() != null) {
-            outcome = researchOutcome.getDescription();
-          }
-          if (outcome.isEmpty()) {
-            outcome = "&lt;Not Defined&gt;";
-          }
-          countOutcome++;
-
-          model.addRow(new Object[] {shortName, title, researchTopicTitle, outcome, showResearchTopic, id});
-        }
-      }
-    }
-
-
-    return model;
-  }
-
-  /**
-   * Get the program impact information
-   * 
-   * @return model with program impact information
-   */
-  private TypedTableModel getProgramImpactTableModel() {
-    // Initialization of Model
-    TypedTableModel model =
-      new TypedTableModel(new String[] {"title", "objectives", "program_id", "research_impact_id", "shortName"},
-        new Class[] {String.class, String.class, Long.class, Long.class, String.class});
-    for (ResearchImpact researchImpact : researchProgram.getResearchImpacts().stream().filter(rp -> rp.isActive())
-      .collect(Collectors.toList())) {
-      String title = "";
-      String objectives = "";
-      long programId = 0;
-      if (researchImpact.getDescription() == null || researchImpact.getDescription().isEmpty()) {
-        title = "&lt;Not Defined&gt;";
-      } else {
-        title = researchImpact.getDescription();
-      }
-
-      if (researchImpact.getResearchImpactObjectives() != null
-        && researchImpact.getResearchImpactObjectives().size() > 0) {
-        for (ResearchImpactObjective researchImpactObjective : researchImpact.getResearchImpactObjectives().stream()
-          .filter(rio -> rio.isActive() && rio.getResearchObjective() != null).collect(Collectors.toList())) {
-          objectives += "&#9679" + researchImpactObjective.getResearchObjective().getObjective() + "<br>";
-        }
-      } else {
-        objectives = "&lt;Not Defined&gt;";
-      }
-      if (objectives.isEmpty()) {
-        objectives = null;
-      }
-      if (researchProgram != null) {
-        programId = researchProgram.getId();
-      }
-
-      String shortName = "";
-      if (researchImpact.getShortName() == null || researchImpact.getShortName().isEmpty()) {
-        shortName = "&lt;Not Defined&gt;";
-      } else {
-        shortName = researchImpact.getShortName();
-      }
-
-
-      model.addRow(new Object[] {title, objectives, programId, researchImpact.getId(), shortName});
-    }
+    model.addRow(new Object[] {title, currentDate, imageUrl, researchProgram.getId()});
     return model;
   }
 
   public ResearchProgram getResearchProgram() {
     return researchProgram;
-  }
-
-  private TypedTableModel getResearchTopicsTableModel() {
-    // Initialization of Model
-    TypedTableModel model = new TypedTableModel(new String[] {"name", "id"}, new Class[] {String.class, String.class});
-    String name = "";
-    String id = "";
-
-    for (ResearchTopic researchTopic : researchProgram.getResearchTopics().stream()
-      .filter(rt -> rt.isActive() && rt.getResearchTopic() != null).collect(Collectors.toList())) {
-      name = researchTopic.getResearchTopic();
-      id = researchTopic.getId().toString();
-      model.addRow(new Object[] {name, id});
-    }
-    return model;
   }
 
   @Override
@@ -535,8 +276,7 @@ public class ImpactPathwayOutcomesSummaryAction extends BaseAction implements Su
       programID = Long.parseLong(StringUtils.trim(this.getRequest().getParameter(APConstants.CENTER_PROGRAM_ID)));
       researchProgram = programService.getProgramById(programID);
     } catch (Exception e) {
-      LOG.error("Failed to get " + APConstants.CENTER_PROGRAM_ID
-        + " parameter. Parameter will be set as -1. Exception: " + e.getMessage());
+      LOG.error("Failed to get " + APConstants.CENTER_PROGRAM_ID + " parameter. Exception: " + e.getMessage());
     }
     // Calculate time to generate report
     startTime = System.currentTimeMillis();
